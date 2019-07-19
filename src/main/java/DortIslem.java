6a56dcc73db2bc7e88645d5be39b219242065c0b
Murat Utku KABASAKALOĞLU <utkukabasakal58@gmail.com>
2019-07-19 11:44:18 +0300

    Gradle example

---
+++ src/main/java/DortIslem.java
@@ -0,0 +1,14 @@
+public class DortIslem {
+    public float topla(float sayi1, float sayi2) {
+        return sayi1 + sayi2;
+    }
+    public float cikar(float sayi1, float sayi2) {
+        return sayi1 - sayi2;
+    }
+    public float carp(float sayi1, float sayi2) {
+        return sayi1 * sayi2;
+    }
+    public float bol(float sayi1, float sayi2) {
+        return sayi1 / sayi2;
+    }
+}
